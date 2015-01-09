07551170add4766c118d36998b46eb72d30e846f
Adam Sharp <adsharp@me.com>
2015-01-09 11:31:21 +1100

    Add default implementations for Array
    This adds implementations for infix versions of `map`, `apply`, and
    `flatMap` for Array instances.

---
+++ Source/Array.swift
@@ -0,0 +1,21 @@
+public func <^><T, U>(f: T -> U, a: [T]) -> [U] {
+    return a.map(f)
+}
+
+public func <*><T, U>(fs: [T -> U], a: [T]) -> [U] {
+    return a.apply(fs)
+}
+
+public func >>-<T, U>(a: [T], f: T -> [U]) -> [U] {
+    return a.flatMap(f)
+}
+
+extension Array {
+    func apply<U>(fs: [T -> U]) -> [U] {
+        return fs.flatMap { f in self.map(f) }
+    }
+
+    func flatMap<U>(f: T -> [U]) -> [U] {
+        return reduce([]) { bs, a in bs + f(a) }
+    }
+}
