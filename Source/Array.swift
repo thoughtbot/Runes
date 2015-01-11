aca6319606f49024d798597da39dc8a07c62e661
Adam Sharp <adsharp@me.com>
2015-01-12 07:04:03 +1100

    Array functions should be public

--- Source/Array.swift
+++ Source/Array.swift
@@ -1,11 +1,11 @@
-func <^><T, U>(f: T -> U, a: [T]) -> [U] {
+public func <^><T, U>(f: T -> U, a: [T]) -> [U] {
     return a.map(f)
 }
 
-func <*><T, U>(fs: [T -> U], a: [T]) -> [U] {
+public func <*><T, U>(fs: [T -> U], a: [T]) -> [U] {
     return fs >>- { f in a.map(f) }
 }
 
-func >>-<T, U>(a: [T], f: T -> [U]) -> [U] {
+public func >>-<T, U>(a: [T], f: T -> [U]) -> [U] {
     return a.reduce([]) { bs, a in bs + f(a) }
 }
