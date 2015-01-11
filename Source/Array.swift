7d9ab23a00b44b448727ef11d1a0b7c2435fd1bd
Adam Sharp <adsharp@me.com>
2015-01-12 07:03:26 +1100

    Make Array types and parameter names consistent with Optional

--- Source/Array.swift
+++ Source/Array.swift
@@ -1,11 +1,11 @@
-func <^> <S: SequenceType, A, B where S.Generator.Element == A> (f: A -> B, source: S) -> [B] {
-    return map(source, f)
+func <^><T, U>(f: T -> U, a: [T]) -> [U] {
+    return a.map(f)
 }
 
-func <*> <A, B> (fs: [A -> B], source: [A]) -> [B] {
-    return fs >>- { f in f <^> source }
+func <*><T, U>(fs: [T -> U], a: [T]) -> [U] {
+    return fs >>- { f in a.map(f) }
 }
 
-func >>- <S: SequenceType, T: SequenceType, A, B where S.Generator.Element == A, T.Generator.Element == B> (source: S, f: A -> T) -> [B] {
-    return reduce(source, []) { dest, a in dest + f(a) }
+func >>-<T, U>(a: [T], f: T -> [U]) -> [U] {
+    return a.reduce([]) { bs, a in bs + f(a) }
 }
