87738c0a24fee6d8f21fb589ef4c6e5488078813
Ricardo Pereira <m@ricardopereira.eu>
2015-05-24 00:12:22 +0100

    Updated >>- precedence

--- Source/Operators.swift
+++ Source/Operators.swift
@@ -1,4 +1,4 @@
 infix operator <^> { associativity left precedence 130 }
 infix operator <*> { associativity left precedence 130 }
-infix operator >>- { associativity left precedence 100 }
+infix operator >>- { associativity left precedence 135 }
 infix operator -<< { associativity right precedence 100 }
