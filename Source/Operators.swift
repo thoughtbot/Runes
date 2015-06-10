170b790d2ccbd5fea766e6c978440883de2a1b8d
Gordon Fontenot <gordon@thoughtbot.com>
2015-06-10 10:33:38 -0700

    Rename Operators -> Runes
    Also add documentation to operator definitions

--- Source/Operators.swift
+++
@@ -1,4 +0,0 @@
-infix operator <^> { associativity left precedence 130 }
-infix operator <*> { associativity left precedence 130 }
-infix operator >>- { associativity left precedence 100 }
-infix operator -<< { associativity right precedence 100 }
