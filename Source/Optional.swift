bc41a43fb535a025c5a78443c0b4c69135ec8396
Gordon Fontenot <gordon@thoughtbot.com>
2015-06-10 10:01:48 -0700

    wip

--- Source/Optional.swift
+++ Source/Optional.swift
@@ -4,10 +4,10 @@
     - If the value is .None, the function will not be evaluated and this will return .None
     - If the value is .Some, the function will be applied to the unwrapped value
 
-    :param: f A transformation function from type T to type U
-    :param: a A value of type Optional<T>
+    - parameter f: A transformation function from type T to type U
+    - parameter a: A value of type Optional<T>
 
-    :returns: A value of type Optional<U>
+    - returns: A value of type Optional<U>
 */
 public func <^><T, U>(@noescape f: T -> U, a: T?) -> U? {
     return a.map(f)
@@ -19,10 +19,10 @@ public func <^><T, U>(@noescape f: T -> U, a: T?) -> U? {
     - If either the value or the function are .None, the function will not be evaluated and this will return .None
     - If both the value and the function are .Some, the function will be applied to the unwrapped value
 
-    :param: f An optional transformation function from type T to type U
-    :param: a A value of type Optional<T>
+    - parameter f: An optional transformation function from type T to type U
+    - parameter a: A value of type Optional<T>
 
-    :returns: A value of type Optional<U>
+    - returns: A value of type Optional<U>
 */
 public func <*><T, U>(f: (T -> U)?, a: T?) -> U? {
     return a.apply(f)
@@ -34,10 +34,10 @@ public func <*><T, U>(f: (T -> U)?, a: T?) -> U? {
     - If the value is .None, the function will not be evaluated and this will return .None
     - If the value is .Some, the function will be applied to the unwrapped value
 
-    :param: f A transformation function from type T to type Optional<U>
-    :param: a A value of type Optional<T>
+    - parameter f: A transformation function from type T to type Optional<U>
+    - parameter a: A value of type Optional<T>
 
-    :returns: A value of type Optional<U>
+    - returns: A value of type Optional<U>
 */
 public func >>-<T, U>(a: T?, @noescape f: T -> U?) -> U? {
     return a.flatMap(f)
@@ -49,10 +49,10 @@ flatMap a function over an optional value (right associative)
 - If the value is .None, the function will not be evaluated and this will return .None
 - If the value is .Some, the function will be applied to the unwrapped value
 
-:param: a A value of type Optional<T>
-:param: f A transformation function from type T to type Optional<U>
+- parameter a: A value of type Optional<T>
+- parameter f: A transformation function from type T to type Optional<U>
 
-:returns: A value of type Optional<U>
+- returns: A value of type Optional<U>
 */
 public func -<<<T, U>(@noescape f: T -> U?, a: T?) -> U? {
   return a.flatMap(f)
@@ -61,9 +61,9 @@ public func -<<<T, U>(@noescape f: T -> U?, a: T?) -> U? {
 /**
     Wrap a value in a minimal context of .Some
 
-    :param: a A value of type T
+    - parameter a: A value of type T
 
-    :returns: The provided value wrapped in .Some
+    - returns: The provided value wrapped in .Some
 */
 public func pure<T>(a: T) -> T? {
     return .Some(a)
@@ -76,9 +76,9 @@ extension Optional {
         - If either self or the function are .None, the function will not be evaluated and this will return .None
         - If both self and the function are .Some, the function will be applied to the unwrapped value
 
-        :param: f An optional transformation function from type T to type U
+        - parameter f: An optional transformation function from type T to type U
 
-        :returns: A value of type Optional<U>
+        - returns: A value of type Optional<U>
     */
     func apply<U>(f: (T -> U)?) -> U? {
         return f.flatMap { self.map($0) }
