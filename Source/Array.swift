ac47a75635fa732d9e80a6d45fb600b7028fea2b
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-07-13 17:08:27 -0400

    Update documentation for Swift 2.0/Xcode 7

--- Source/Array.swift
+++ Source/Array.swift
@@ -3,10 +3,10 @@
 
     This will return a new array resulting from the transformation function beind applied to each value in the array
 
-    :param: f A transformation function from type T to type U
-    :param: a A value of type [T]
+    - parameter f: A transformation function from type T to type U
+    - parameter a: A value of type [T]
 
-    :returns: A value of type [U]
+    - returns: A value of type [U]
 */
 public func <^><T, U>(f: T -> U, a: [T]) -> [U] {
     return a.map(f)
@@ -17,10 +17,10 @@ public func <^><T, U>(f: T -> U, a: [T]) -> [U] {
 
     This will return a new array resulting from the matrix of each function being applied to each value in the array
 
-    :param: fs An array of transformation functions from type T to type U
-    :param: a A value of type [T]
+    - parameter fs: An array of transformation functions from type T to type U
+    - parameter a: A value of type [T]
 
-    :returns: A value of type [U]
+    - returns: A value of type [U]
 */
 public func <*><T, U>(fs: [T -> U], a: [T]) -> [U] {
     return a.apply(fs)
@@ -31,10 +31,10 @@ public func <*><T, U>(fs: [T -> U], a: [T]) -> [U] {
 
     apply a function to each value of an array and flatten the resulting array
 
-    :param: f A transformation function from type T to type [U]
-    :param: a A value of type [T]
+    - parameter f: A transformation function from type T to type [U]
+    - parameter a: A value of type [T]
 
-    :returns: A value of type [U]
+    - returns: A value of type [U]
 */
 public func >>-<T, U>(a: [T], @noescape f: T -> [U]) -> [U] {
     return a.flatMap(f)
@@ -45,10 +45,10 @@ flatMap a function over an array of values (right associative)
 
 apply a function to each value of an array and flatten the resulting array
 
-:param: f A transformation function from type T to type [U]
-:param: a A value of type [T]
+- parameter f: A transformation function from type T to type [U]
+- parameter a: A value of type [T]
 
-:returns: A value of type [U]
+- returns: A value of type [U]
 */
 public func -<<<T, U>(@noescape f: T -> [U], a: [T]) -> [U] {
   return a.flatMap(f)
@@ -57,9 +57,9 @@ public func -<<<T, U>(@noescape f: T -> [U], a: [T]) -> [U] {
 /**
     Wrap a value in a minimal context of []
 
-    :param: a A value of type T
+    - parameter a: A value of type T
 
-    :returns: The provided value wrapped in an array
+    - returns: The provided value wrapped in an array
 */
 public func pure<T>(a: T) -> [T] {
     return [a]
@@ -71,9 +71,9 @@ extension Array {
 
         This will return a new array resulting from the matrix of each function being applied to each value inside self
 
-        :param: fs An array of transformation functions from type T to type U
+        - parameter fs: An array of transformation functions from type T to type U
 
-        :returns: A value of type [U]
+        - returns: A value of type [U]
     */
     func apply<U>(fs: [T -> U]) -> [U] {
         return fs.flatMap { self.map($0) }
