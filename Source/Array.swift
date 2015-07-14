057116afb8bf148d7c1bf5a057e476ea629d5bd1
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-07-14 12:16:47 -0400

    Update documentation with code blocks
    Since Swift 2.0 uses markdown for documentation parsing, we can use
    backticks to indicate code blocks. Which is neat.

--- Source/Array.swift
+++ Source/Array.swift
@@ -3,10 +3,10 @@
 
     This will return a new array resulting from the transformation function beind applied to each value in the array
 
-    - parameter f: A transformation function from type T to type U
-    - parameter a: A value of type [T]
+    - parameter f: A transformation function from type `T` to type `U`
+    - parameter a: A value of type `[T]`
 
-    - returns: A value of type [U]
+    - returns: A value of type `[U]`
 */
 public func <^><T, U>(f: T -> U, a: [T]) -> [U] {
     return a.map(f)
@@ -17,10 +17,10 @@ public func <^><T, U>(f: T -> U, a: [T]) -> [U] {
 
     This will return a new array resulting from the matrix of each function being applied to each value in the array
 
-    - parameter fs: An array of transformation functions from type T to type U
-    - parameter a: A value of type [T]
+    - parameter fs: An array of transformation functions from type `T` to type `U`
+    - parameter a: A value of type `[T]`
 
-    - returns: A value of type [U]
+    - returns: A value of type `[U]`
 */
 public func <*><T, U>(fs: [T -> U], a: [T]) -> [U] {
     return a.apply(fs)
@@ -31,33 +31,33 @@ public func <*><T, U>(fs: [T -> U], a: [T]) -> [U] {
 
     apply a function to each value of an array and flatten the resulting array
 
-    - parameter f: A transformation function from type T to type [U]
-    - parameter a: A value of type [T]
+    - parameter f: A transformation function from type `T` to type `[U]`
+    - parameter a: A value of type `[T]`
 
-    - returns: A value of type [U]
+    - returns: A value of type `[U]`
 */
 public func >>-<T, U>(a: [T], @noescape f: T -> [U]) -> [U] {
     return a.flatMap(f)
 }
 
 /**
-flatMap a function over an array of values (right associative)
+    flatMap a function over an array of values (right associative)
 
-apply a function to each value of an array and flatten the resulting array
+    apply a function to each value of an array and flatten the resulting array
 
-- parameter f: A transformation function from type T to type [U]
-- parameter a: A value of type [T]
+    - parameter f: A transformation function from type `T` to type `[U]`
+    - parameter a: A value of type `[T]`
 
-- returns: A value of type [U]
+    - returns: A value of type `[U]`
 */
 public func -<<<T, U>(@noescape f: T -> [U], a: [T]) -> [U] {
   return a.flatMap(f)
 }
 
 /**
-    Wrap a value in a minimal context of []
+    Wrap a value in a minimal context of `[]`
 
-    - parameter a: A value of type T
+    - parameter a: A value of type `T`
 
     - returns: The provided value wrapped in an array
 */
@@ -67,13 +67,13 @@ public func pure<T>(a: T) -> [T] {
 
 public extension Array {
     /**
-        apply an array of functions to self
+        apply an array of functions to `self`
 
-        This will return a new array resulting from the matrix of each function being applied to each value inside self
+        This will return a new array resulting from the matrix of each function being applied to each value inside `self`
 
-        - parameter fs: An array of transformation functions from type Element to type U
+        - parameter fs: An array of transformation functions from type `Element` to type `U`
 
-        - returns: A value of type [U]
+        - returns: A value of type `[U]`
     */
     func apply<U>(fs: [Element -> U]) -> [U] {
         return fs.flatMap { self.map($0) }
--- Source/Optional.swift
+++ Source/Optional.swift
@@ -1,13 +1,13 @@
 /**
     map a function over an optional value
 
-    - If the value is .None, the function will not be evaluated and this will return .None
-    - If the value is .Some, the function will be applied to the unwrapped value
+    - If the value is `.None`, the function will not be evaluated and this will return `.None`
+    - If the value is `.Some`, the function will be applied to the unwrapped value
 
-    - parameter f: A transformation function from type T to type U
-    - parameter a: A value of type Optional<T>
+    - parameter f: A transformation function from type `T` to type `U`
+    - parameter a: A value of type `Optional<T>`
 
-    - returns: A value of type Optional<U>
+    - returns: A value of type `Optional<U>`
 */
 public func <^><T, U>(@noescape f: T -> U, a: T?) -> U? {
     return a.map(f)
@@ -16,13 +16,13 @@ public func <^><T, U>(@noescape f: T -> U, a: T?) -> U? {
 /**
     apply an optional function to an optional value
 
-    - If either the value or the function are .None, the function will not be evaluated and this will return .None
-    - If both the value and the function are .Some, the function will be applied to the unwrapped value
+    - If either the value or the function are `.None`, the function will not be evaluated and this will return `.None`
+    - If both the value and the function are `.Some`, the function will be applied to the unwrapped value
 
-    - parameter f: An optional transformation function from type T to type U
-    - parameter a: A value of type Optional<T>
+    - parameter f: An optional transformation function from type `T` to type `U`
+    - parameter a: A value of type `Optional<T>`
 
-    - returns: A value of type Optional<U>
+    - returns: A value of type `Optional<U>`
 */
 public func <*><T, U>(f: (T -> U)?, a: T?) -> U? {
     return a.apply(f)
@@ -31,39 +31,39 @@ public func <*><T, U>(f: (T -> U)?, a: T?) -> U? {
 /**
     flatMap a function over an optional value (left associative)
 
-    - If the value is .None, the function will not be evaluated and this will return .None
-    - If the value is .Some, the function will be applied to the unwrapped value
+    - If the value is `.None`, the function will not be evaluated and this will return `.None`
+    - If the value is `.Some`, the function will be applied to the unwrapped value
 
-    - parameter f: A transformation function from type T to type Optional<U>
-    - parameter a: A value of type Optional<T>
+    - parameter f: A transformation function from type `T` to type `Optional<U>`
+    - parameter a: A value of type `Optional<T>`
 
-    - returns: A value of type Optional<U>
+    - returns: A value of type `Optional<U>`
 */
 public func >>-<T, U>(a: T?, @noescape f: T -> U?) -> U? {
     return a.flatMap(f)
 }
 
 /**
-flatMap a function over an optional value (right associative)
+    flatMap a function over an optional value (right associative)
 
-- If the value is .None, the function will not be evaluated and this will return .None
-- If the value is .Some, the function will be applied to the unwrapped value
+    - If the value is `.None`, the function will not be evaluated and this will return `.None`
+    - If the value is `.Some`, the function will be applied to the unwrapped value
 
-- parameter a: A value of type Optional<T>
-- parameter f: A transformation function from type T to type Optional<U>
+    - parameter a: A value of type `Optional<T>`
+    - parameter f: A transformation function from type `T` to type `Optional<U>`
 
-- returns: A value of type Optional<U>
+    - returns: A value of type `Optional<U>`
 */
 public func -<<<T, U>(@noescape f: T -> U?, a: T?) -> U? {
   return a.flatMap(f)
 }
 
 /**
-    Wrap a value in a minimal context of .Some
+    Wrap a value in a minimal context of `.Some`
 
-    - parameter a: A value of type T
+    - parameter a: A value of type `T`
 
-    - returns: The provided value wrapped in .Some
+    - returns: The provided value wrapped in `.Some`
 */
 public func pure<T>(a: T) -> T? {
     return .Some(a)
@@ -71,14 +71,14 @@ public func pure<T>(a: T) -> T? {
 
 public extension Optional {
     /**
-        apply an optional function to self
+        apply an optional function to `self`
 
-        - If either self or the function are .None, the function will not be evaluated and this will return .None
-        - If both self and the function are .Some, the function will be applied to the unwrapped value
+        - If either self or the function are `.None`, the function will not be evaluated and this will return `.None`
+        - If both self and the function are `.Some`, the function will be applied to the unwrapped value
 
-        - parameter f: An optional transformation function from type T to type U
+        - parameter f: An optional transformation function from type `T` to type `U`
 
-        - returns: A value of type Optional<U>
+        - returns: A value of type `Optional<U>`
     */
     func apply<U>(f: (T -> U)?) -> U? {
         return f.flatMap { self.map($0) }
