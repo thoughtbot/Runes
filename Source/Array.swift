be3dc7f22dd859c47a1388d4ee64c014c05e898e
jdhealy <jdhealy@gmail.com>
2015-08-20 18:47:09 -0400

    Typo: fix “beind” → “being”.

--- Source/Array.swift
+++ Source/Array.swift
@@ -1,7 +1,7 @@
 /**
     map a function over an array of values
 
-    This will return a new array resulting from the transformation function beind applied to each value in the array
+    This will return a new array resulting from the transformation function being applied to each value in the array
 
     - parameter f: A transformation function from type `T` to type `U`
     - parameter a: A value of type `[T]`
