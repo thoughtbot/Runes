9ae81faa422a14920306fd75103583e65b7aabd1
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-09-11 18:27:17 -0400

    wip - Move from Fox to SwiftCheck
    I really like SwiftCheck's syntax more then Fox. And this removes a
    metric fuckload of overhead, since it becomes the only dependency.

--- Tests/Tests/ArraySpec.swift
+++ Tests/Tests/ArraySpec.swift
@@ -1,131 +1,83 @@
-import Fox
-import Nimble
-import NimbleFox
-import Quick
+import XCTest
+import SwiftCheck
 import Runes
 
-private func generateArray(block:[String] -> Bool) -> FOXGenerator {
-    return forAll(array(string())) { array in
-        return block(array as! [String])
+class ArraySpec: XCTestCase {
+    func testFunctor() {
+        // fmap id = id
+        property("obeys the identity law") <- forAll { (array: [String]) in
+            let lhs = id <^> array
+            let rhs = array
+
+            return lhs == rhs
+        }
+
+        // fmap (g . h) = (fmap g) . (fmap h)
+        property("obeys the function composition law") <- forAll { (array: [String]) in
+            let lhs = compose(append, prepend) <^> array
+            let rhs = compose(curry(<^>)(append), curry(<^>)(prepend))(array)
+
+            return lhs == rhs
+        }
     }
-}
 
-class ArraySpec: QuickSpec {
-    override func spec() {
-        describe("Array") {
-            describe("map") {
-                // fmap id = id
-                it("obeys the identity law") {
-                    let property = generateArray() { array in
-                        let lhs = id <^> array
-                        let rhs = array
+    func testApplicative() {
+        // pure id <*> v = v
+        property("obeys the identity law") <- forAll { (array: [String]) in
+            let lhs = pure(id) <*> array
+            let rhs = array
 
-                        return lhs == rhs
-                    }
+            return lhs == rhs
+        }
 
-                    expect(property).to(hold())
-                }
+        // pure f <*> pure x = pure (f x)
+        property("obeys the homomorphism law") <- forAll { (string: String) in
+            let lhs: [String] = pure(append) <*> pure(string)
+            let rhs: [String] = pure(append(string))
 
-                // fmap (g . h) = (fmap g) . (fmap h)
-                it("obeys the function composition law") {
-                    let property = generateArray() { array in
-                        let lhs = compose(append, prepend) <^> array
-                        let rhs = compose(curry(<^>)(append), curry(<^>)(prepend))(array)
+            return rhs == lhs
+        }
 
-                        return lhs == rhs
-                    }
+        // u <*> pure y = pure ($ y) <*> u
+        property("obeys the interchange law") <- forAll { (string: String) in
+            let lhs: [String] = pure(append) <*> pure(string)
+            let rhs: [String] = pure({ $0(string) }) <*> pure(append)
 
-                    expect(property).to(hold())
-                }
-            }
+            return lhs == rhs
+        }
 
-            describe("apply") {
-                // pure id <*> v = v
-                it("obeys the identity law") {
-                    let property = generateArray() { array in
-                        let lhs = pure(id) <*> array
-                        let rhs = array
+        // u <*> (v <*> w) = pure (.) <*> u <*> v <*> w
+        property("obeys the composition law") <- forAll { (array: [String]) in
+            let lhs = pure(append) <*> (pure(prepend) <*> array)
+            let rhs = pure(curry(compose)) <*> pure(append)  <*> pure(prepend) <*> array
 
-                        return lhs == rhs
-                    }
+            return lhs == rhs
+        }
+    }
 
-                    expect(property).to(hold())
-                }
+    func testMonad() {
+        // return x >>= f = f x
+        property("obeys the left identity law") <- forAll { (string: String) in
+            let lhs: [String] = pure(string) >>- compose(append, pure)
+            let rhs: [String] = compose(append, pure)(string)
 
-                // pure f <*> pure x = pure (f x)
-                it("obeys the homomorphism law") {
-                    let property = generateString() { string in
-                        let lhs: [String] = pure(append) <*> pure(string)
-                        let rhs: [String] = pure(append(string))
+            return lhs == rhs
+        }
 
-                        return rhs == lhs
-                    }
+        // m >>= return = m
+        property("obeys the right identity law") <- forAll { (array: [String]) in
+            let lhs = array >>- pure
+            let rhs = array
 
-                    expect(property).to(hold())
-                }
+            return lhs == rhs
+        }
 
-                // u <*> pure y = pure ($ y) <*> u
-                it("obeys the interchange law") {
-                    let property = generateString() { string in
-                        let lhs: [String] = pure(append) <*> pure(string)
-                        let rhs: [String] = pure({ $0(string) }) <*> pure(append)
+        // (m >>= f) >>= g = m >>= (\x -> f x >>= g)
+        property("obeys the associativity law") <- forAll { (array: [String]) in
+            let lhs = (array >>- compose(append, pure)) >>- compose(prepend, pure)
+            let rhs = array >>- { x in compose(append, pure)(x) >>- compose(prepend, pure) }
 
-                        return lhs == rhs
-                    }
-
-                    expect(property).to(hold())
-                }
-
-                // u <*> (v <*> w) = pure (.) <*> u <*> v <*> w
-                it("obeys the composition law") {
-                    let property = generateArray() { array in
-                        let lhs = pure(append) <*> (pure(prepend) <*> array)
-                        let rhs = pure(curry(compose)) <*> pure(append)  <*> pure(prepend) <*> array
-
-                        return lhs == rhs
-                    }
-
-                    expect(property).to(hold())
-                }
-            }
-
-            describe("flatMap") {
-                // return x >>= f = f x
-                it("obeys the left identity law") {
-                    let property = generateString() { string in
-                        let lhs: [String] = pure(string) >>- compose(append, pure)
-                        let rhs: [String] = compose(append, pure)(string)
-
-                        return lhs == rhs
-                    }
-
-                    expect(property).to(hold())
-                }
-
-                // m >>= return = m
-                it("obeys the right identity law") {
-                    let property = generateArray() { array in
-                        let lhs = array >>- pure
-                        let rhs = array
-
-                        return lhs == rhs
-                    }
-
-                    expect(property).to(hold())
-                }
-
-                // (m >>= f) >>= g = m >>= (\x -> f x >>= g)
-                it("obeys the associativity law") {
-                    let property = generateArray() { array in
-                        let lhs = (array >>- compose(append, pure)) >>- compose(prepend, pure)
-                        let rhs = array >>- { x in compose(append, pure)(x) >>- compose(prepend, pure) }
-
-                        return lhs == rhs
-                    }
-
-                    expect(property).to(hold())
-                }
-            }
+            return lhs == rhs
         }
     }
 }
