d672f25b43356dc8230f33353e6564a5006ed80c
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-09-16 20:58:09 -0400

    Generate functions for optional spec

--- Tests/Tests/OptionalSpec.swift
+++ Tests/Tests/OptionalSpec.swift
@@ -5,17 +5,21 @@ import Runes
 class OptionalSpec: XCTestCase {
     func testFunctor() {
         // fmap id = id
-        property("identity law") <- forAll { (optional: String?) in
-            let lhs = id <^> optional
-            let rhs = optional
+        property("identity law") <- forAll { (x: String?) in
+            let lhs = id <^> x
+            let rhs = x
 
             return lhs == rhs
         }
 
-        // fmap (g . h) = (fmap g) . (fmap h)
-        property("function composition law") <- forAll { (optional: String?) in
-            let lhs = compose(append, prepend) <^> optional
-            let rhs = compose(curry(<^>)(append), curry(<^>)(prepend))(optional)
+        // fmap (f . g) = (fmap f) . (fmap g)
+        property("function composition law") <- forAll { (o: OptionalOf<String>, fa: ArrowOf<String, String>, fb: ArrowOf<String, String>) in
+            let f = fa.getArrow
+            let g = fb.getArrow
+            let x = o.getOptional
+
+            let lhs = compose(f, g) <^> x
+            let rhs = compose(curry(<^>)(f), curry(<^>)(g))(x)
 
             return lhs == rhs
         }
@@ -23,33 +27,41 @@ class OptionalSpec: XCTestCase {
 
     func testApplicative() {
         // pure id <*> v = v
-        property("identity law") <- forAll { (optional: String?) in
-            let lhs = pure(id) <*> optional
-            let rhs = optional
+        property("identity law") <- forAll { (x: String?) in
+            let lhs = pure(id) <*> x
+            let rhs = x
 
             return lhs == rhs
         }
 
         // pure f <*> pure x = pure (f x)
-        property("homomorphism law") <- forAll { (string: String) in
-            let lhs: String? = pure(append) <*> pure(string)
-            let rhs: String? = pure(append(string))
+        property("homomorphism law") <- forAll { (x: String, fa: ArrowOf<String, String>) in
+            let f = fa.getArrow
+
+            let lhs: String? = pure(f) <*> pure(x)
+            let rhs: String? = pure(f(x))
 
             return rhs == lhs
         }
 
-        // u <*> pure y = pure ($ y) <*> u
-        property("interchange law") <- forAll { (string: String) in
-            let lhs: String? = pure(append) <*> pure(string)
-            let rhs: String? = pure({ $0(string) }) <*> pure(append)
+        // f <*> pure x = pure ($ x) <*> f
+        property("interchange law") <- forAll { (x: String, fa: OptionalOf<ArrowOf<String, String>>) in
+            let f = fa.getOptional?.getArrow
+
+            let lhs = f <*> pure(x)
+            let rhs = pure({ $0(x) }) <*> f
 
             return lhs == rhs
         }
 
-        // u <*> (v <*> w) = pure (.) <*> u <*> v <*> w
-        property("compospropertyion law") <- forAll { (optional: String?) in
-            let lhs = pure(append) <*> (pure(prepend) <*> optional)
-            let rhs = pure(curry(compose)) <*> pure(append)  <*> pure(prepend) <*> optional
+        // f <*> (g <*> x) = pure (.) <*> f <*> g <*> x
+        property("composition law") <- forAll { (o: OptionalOf<String>, fa: OptionalOf<ArrowOf<String, String>>, fb: OptionalOf<ArrowOf<String, String>>) in
+            let x = o.getOptional
+            let f = fa.getOptional?.getArrow
+            let g = fb.getOptional?.getArrow
+
+            let lhs = f <*> (g <*> x)
+            let rhs = pure(curry(compose)) <*> f <*> g <*> x
 
             return lhs == rhs
         }
@@ -57,25 +69,33 @@ class OptionalSpec: XCTestCase {
 
     func testMonad() {
         // return x >>= f = f x
-        property("left identity law") <- forAll { (string: String) in
-            let lhs: String? = pure(string) >>- compose(append, pure)
-            let rhs: String? = compose(append, pure)(string)
+        property("left identity law") <- forAll { (x: String, fa: ArrowOf<String, String>) in
+            let f: String -> String? = compose(fa.getArrow, pure)
+
+            let lhs = pure(x) >>- f
+            let rhs = f(x)
 
             return lhs == rhs
         }
 
         // m >>= return = m
-        property("right identity law") <- forAll { (optional: String?) in
-            let lhs = optional >>- pure
-            let rhs = optional
+        property("right identity law") <- forAll { (o: OptionalOf<String>) in
+            let x = o.getOptional
+
+            let lhs = x >>- pure
+            let rhs = x
 
             return lhs == rhs
         }
 
         // (m >>= f) >>= g = m >>= (\x -> f x >>= g)
-        property("associativity law") <- forAll { (optional: String?) in
-            let lhs = (optional >>- compose(append, pure)) >>- compose(prepend, pure)
-            let rhs = optional >>- { x in compose(append, pure)(x) >>- compose(prepend, pure) }
+        property("associativity law") <- forAll { (o: OptionalOf<String>, fa: ArrowOf<String, String>, fb: ArrowOf<String, String>) in
+            let m = o.getOptional
+            let f: String -> String? = compose(fa.getArrow, pure)
+            let g: String -> String? = compose(fb.getArrow, pure)
+
+            let lhs = (m >>- f) >>- g
+            let rhs = m >>- { x in f(x) >>- g }
 
             return lhs == rhs
         }
