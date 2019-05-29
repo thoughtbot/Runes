6eb0cffe369436473427d275defd7d8c4805e2ea
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2019-05-29 12:34:39 -0500

    Add SwiftCheck dependency to Package.swift
    Since we're pointing at an actual release now, we can declare this dependency
    for Swift Package Manager again. This re-enables the ability to run `swift
    test`!

--- Package.swift
+++ Package.swift
@@ -7,7 +7,17 @@ let package = Package(
   products: [
     .library(name: "Runes", targets: ["Runes"])
   ],
+  dependencies: [
+    .package(url: "git@github.com:typelift/SwiftCheck.git", from: "0.12.0")
+  ],
   targets: [
     .target(name: "Runes"),
+    .testTarget(
+      name: "RunesTests",
+      dependencies: [
+        "Runes",
+        "SwiftCheck",
+      ]
+    ),
   ]
 )
